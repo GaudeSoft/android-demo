1191368c3f5e11cd3f1bb423c90e5c7cc6d3f7ed
HOUNKPEVI Gautier <gaudesoft1999@gmail.com>
2021-11-22 19:03:30 +0100

    deuxième modification

---
+++ app/src/main/java/com/example/gaudesoftdemo/Calculatrice.java
@@ -0,0 +1,14 @@
+package com.example.gaudesoftdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculatrice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
